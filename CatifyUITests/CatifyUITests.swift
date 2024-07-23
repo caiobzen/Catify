76d6c00f2c90c47310d7fff3ed33f43d8eb01269
Carlos Correa <caiobzen@gmail.com>
2024-07-23 23:38:02 +0100

    refactor: improve UI tests by encapsulating actions and expectations inside the robot

--- CatifyUITests/CatifyUITests.swift
+++ CatifyUITests/CatifyUITests.swift
@@ -9,62 +9,46 @@ final class CatifyUITests: XCTestCase {
         let app = XCUIApplication()
         robot = CatifyRobot(app: XCUIApplication())
         app.launch()
-        
-        let _ = robot.catsList.waitForExistence(timeout: 10)
+        robot.waitCatsListToAppear()
     }
     
     func test_onSearchingForAValidBreed_ItFiltersTheList() throws {
-        
-        robot.searchTextField.tap()
-        robot.searchTextField.typeText("Abyss")
-        XCTAssertEqual(robot.cells.count, 13)
+        robot
+            .searchBy(text: "Abyss")
+            .expectNumberOfCells(toBe: 13)
     }
     
     func test_onSearchingForAnInvalidBreed_ItShowsAMessage() throws {
-        
-        robot.searchTextField.tap()
-        robot.searchTextField.typeText("dog")
-        let exists = robot.noBreedResults.waitForExistence(timeout: 2)
-        XCTAssertTrue(exists)
-        XCTAssertEqual(robot.noBreedResults.label, "No breed named \"dog\"")
+        robot
+            .searchBy(text: "dog")
+            .waitForNoBreedResultsToAppear()
+            .expectNoBreedResultsText(toBe: "No breed named \"dog\"")
     }
     
     func test_onFavoritingACat_itTogglesTheFavoriteButton_andGoesToFavoritesScreen() {
-        
-        let firstFavoriteButton = robot.favoriteButtons.firstMatch
-        firstFavoriteButton.tap()
-        
-        robot.favoritesTab.tap()
-        
-        let _ = robot.catsList.waitForExistence(timeout: 10)
-        XCTAssertEqual(robot.catImages.count, 1)
-        firstFavoriteButton.tap()
-        XCTAssertEqual(robot.catImages.count, 0)
+        robot
+            .favoriteFirstCat()
+            .tapOnFavoritesTab()
+            .waitCatsListToAppear()
+            .expectNumberOfCats(toBe: 1)
+            .favoriteFirstCat()
+            .expectNumberOfCats(toBe: 0)
     }
     
     func test_catDetailsScreen_hasAllTheRequiredInformation() {
-        robot.catImages.firstMatch.tap()
-        
-        let imageExists = robot.catImage.waitForExistence(timeout: 1)
-        let originExists = robot.screenTexts["Origin"].waitForExistence(timeout: 0.1)
-        let temperamentExists = robot.screenTexts["Temperament"].waitForExistence(timeout: 0.1)
-        let descriptionExists = robot.screenTexts["Description"].waitForExistence(timeout: 0.1)
-        
-        XCTAssertTrue(imageExists)
-        XCTAssertTrue(originExists)
-        XCTAssertTrue(temperamentExists)
-        XCTAssertTrue(descriptionExists)
+        robot
+            .selectFirstCat()
+            .expectDetailInformationsToExist()
     }
     
     func test_catDetailsScreen_canFavoriteACat() {
-        robot.catImages.firstMatch.tap()
-        
-        robot.favoriteButtons.firstMatch.tap()
-        robot.dismissView()
-        robot.favoritesTab.tap()
-        
-        XCTAssertEqual(robot.catImages.count, 1)
-        robot.favoriteButtons.firstMatch.tap()
-        XCTAssertEqual(robot.catImages.count, 0)
+        robot
+            .selectFirstCat()
+            .favoriteFirstCat()
+            .dismissView()
+            .tapOnFavoritesTab()
+            .expectNumberOfCats(toBe: 1)
+            .favoriteFirstCat()
+            .expectNumberOfCats(toBe: 0)
     }
 }
--- CatifyUITests/Screens/CatifyRobot.swift
+++ CatifyUITests/Screens/CatifyRobot.swift
@@ -16,7 +16,7 @@ struct CatifyRobot {
     }
     
     private let app: XCUIApplication
-
+    
     init(app: XCUIApplication) {
         self.app = app
     }
@@ -56,8 +56,98 @@ struct CatifyRobot {
     var screenTexts: XCUIElementQuery {
         app.staticTexts
     }
+}
+
+// MARK: - Actions
+extension CatifyRobot {
     
-    func dismissView() {
+    @discardableResult
+    func dismissView() -> Self {
         app.buttons.firstMatch.tap()
+        return self
+    }
+    
+    @discardableResult
+    func searchBy(text: String) -> Self {
+        searchTextField.tap()
+        searchTextField.typeText(text)
+        return self
+    }
+    
+    @discardableResult
+    func favoriteFirstCat() -> Self {
+        favoriteButtons.firstMatch.tap()
+        return self
+    }
+    
+    @discardableResult
+    func tapOnFavoritesTab() -> Self {
+        favoritesTab.tap()
+        return self
+    }
+    
+    @discardableResult
+    func selectFirstCat() -> Self {
+        catImages.firstMatch.tap()
+        return self
+    }
+}
+
+// MARK: - Expections
+extension CatifyRobot {
+
+    @discardableResult
+    func expectNumberOfCells(toBe expectedNumberOfCells: Int, 
+                             file: StaticString = #file,
+                             line: UInt = #line) -> Self {
+        XCTAssertEqual(cells.count, expectedNumberOfCells, file: file, line: line)
+        return self
+    }
+    
+    @discardableResult
+    func expectNumberOfCats(toBe expectedNumberOfCats: Int,
+                            file: StaticString = #file,
+                            line: UInt = #line) -> Self {
+        XCTAssertEqual(catImages.count, expectedNumberOfCats, file: file, line: line)
+        return self
+    }
+    
+    @discardableResult
+    func waitForNoBreedResultsToAppear(file: StaticString = #file,
+                                       line: UInt = #line) -> Self {
+        let exists = noBreedResults.waitForExistence(timeout: 2)
+        XCTAssertTrue(exists, file: file, line: line)
+        return self
+    }
+    
+    @discardableResult
+    func expectNoBreedResultsText(toBe text: String,
+                                  file: StaticString = #file,
+                                  line: UInt = #line) -> Self {
+        XCTAssertEqual(noBreedResults.label, text, file: file, line: line)
+        return self
+    }
+    
+    @discardableResult
+    func waitCatsListToAppear(file: StaticString = #file,
+                              line: UInt = #line) -> Self {
+        let exists = catsList.waitForExistence(timeout: 10)
+        XCTAssertTrue(exists, file: file, line: line)
+        return self
+    }
+    
+    @discardableResult
+    func expectDetailInformationsToExist(file: StaticString = #file,
+                                         line: UInt = #line) -> Self {
+        let imageExists = catImage.waitForExistence(timeout: 1)
+        let originExists = screenTexts["Origin"].waitForExistence(timeout: 0.1)
+        let temperamentExists = screenTexts["Temperament"].waitForExistence(timeout: 0.1)
+        let descriptionExists = screenTexts["Description"].waitForExistence(timeout: 0.1)
+        
+        XCTAssertTrue(imageExists, file: file, line: line)
+        XCTAssertTrue(originExists, file: file, line: line)
+        XCTAssertTrue(temperamentExists, file: file, line: line)
+        XCTAssertTrue(descriptionExists, file: file, line: line)
+        return self
     }
 }
